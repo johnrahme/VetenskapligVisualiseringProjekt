cea4a1c2d1a50542ba965dc3efcfa8dd95b4d745
Dennis Myrgren <dmyrgren@gmail.com>
2016-10-07 15:04:17 +0200

    Comments and sections

--- Studentportalen/threshold_quakes_by_time.py
+++ Studentportalen/threshold_quakes_by_time.py
@@ -1,30 +1,19 @@
 import ReadPointsCSV
 import vtk
 
+#------------------------Start KeyBoard interface------------------------------
 
-# Define a class for the keyboard interface
 class KeyboardInterface(object):
-    """Keyboard interface.
-
-    Provides a simple keyboard interface for interaction. You may
-    extend this interface with keyboard shortcuts for, e.g., moving
-    the slice plane(s) or manipulating the streamline seedpoints.
-
-    """
-
+   
     def __init__(self):
         self.screenshot_counter = 0
         self.day_in_sec = 86400
-        self.day_counter = 0
-        #self.level_depth = 7
+        self.day_counter = 0        
         self.render_window = None
         self.window2image_filter = None
         self.png_writer = None
-        # Add the extra attributes you need here...
-
+        
     def keypress(self, obj, event):
-        """This function captures keypress events and defines actions for
-        keyboard shortcuts."""
         key = obj.GetKeySym()
         
         if key == "9":
@@ -50,13 +39,10 @@ class KeyboardInterface(object):
             threshold_filter.ThresholdBetween(min_time+(self.day_counter-1)*self.day_in_sec,min_time+self.day_counter*self.day_in_sec)	
             threshold_filter.Update()
             self.render_window.Render()
-            
-        # elif key == "Down":
-            # if self.level_depth > 1:
-                # self.level_depth = self.level_depth-1
-                # planes.SetExtent(0,W,0,H,self.level_depth,self.level_depth)
-                # print(self.level_depth)
-                # self.render_window.Render()
+
+#--------------------------End KeyBoard interface------------------------------
+
+#-----------------------Start Create quake glyphs------------------------------
 
 # Load the earthquake data
 points, strength, time_, = ReadPointsCSV.readPoints("Quake_data.txt")
@@ -67,28 +53,21 @@ min_time, max_time = time_.GetRange()  # in seconds
 strength.SetName("strength")
 time_.SetName("time")
 
-# Create a vtkPolyData object from the earthquake data and specify
-# that "strength" should be the active scalar array
+
 points_polydata = vtk.vtkPolyData()
 points_polydata.SetPoints(points)
 points_polydata.GetPointData().AddArray(strength)
 points_polydata.GetPointData().AddArray(time_)
 points_polydata.GetPointData().SetActiveScalars("strength")
 
-# Threshold the earthquake points to extract all points within a
-# specified time interval.
-#
-# If you do not specify which input array to process, i.e., if you
-# comment out the SetInputArrayToProcess() call, the thresholding will
-# be performed on the active scalar array ("strength", in this case).
+
 threshold_filter = vtk.vtkThresholdPoints()
 threshold_filter.SetInputData(points_polydata)
 threshold_filter.ThresholdBetween(min_time, max_time)
 threshold_filter.SetInputArrayToProcess(0, 0, 0, 0, "time")
 threshold_filter.Update()
 
-# Connect the output of the threshold filter to a vtkGlyph3D filter
-# and proceed with the visualization!
+#-------------------------Start ColorTransfer function------------------------- 
 
 ColorTrans = vtk.vtkColorTransferFunction()
 ColorTrans.SetColorSpaceToRGB()
@@ -96,12 +75,7 @@ ColorTrans.AddRGBPoint(min_strength, 1, 0, 0)
 ColorTrans.AddRGBPoint((max_strength-min_strength)/2,1, 1, 1)
 ColorTrans.AddRGBPoint(max_strength, 0, 0, 1)
 
-outline = vtk.vtkOutlineFilter()
-outline.SetInputData(points_polydata)
-outline_mapper = vtk.vtkPolyDataMapper()
-outline_mapper.SetInputConnection(outline.GetOutputPort())
-outline_actor = vtk.vtkActor()
-outline_actor.SetMapper(outline_mapper)
+#---------------------------End ColorTransfer function------------------------- 
 
 sphere = vtk.vtkSphereSource()
 
@@ -116,17 +90,28 @@ glyph_mapper.SetLookupTable(ColorTrans)
 glyph_actor = vtk.vtkActor()
 glyph_actor.SetMapper(glyph_mapper)
 
+#-----------------------End Create quake glyphs--------------------------------
 
+#-----------------------Start Outline------------------------------------------
+
+outline = vtk.vtkOutlineFilter()
+outline.SetInputData(points_polydata)
+outline_mapper = vtk.vtkPolyDataMapper()
+outline_mapper.SetInputConnection(outline.GetOutputPort())
+outline_actor = vtk.vtkActor()
+outline_actor.SetMapper(outline_mapper)
+
+#-------------------------End Outline------------------------------------------
+
+#------------------------Start Renderer----------------------------------------
 renderer = vtk.vtkRenderer()
-renderer.SetBackground(0, 0, 0)
+renderer.SetBackground(0.1, 0.1, 0.1)
 renderer.AddActor(outline_actor)
 renderer.AddActor(glyph_actor)
-#renderer.AddActor(tubes_actor)
-#renderer.AddActor(legendBar)
 
 # Create a render window
 render_window = vtk.vtkRenderWindow()
-render_window.SetWindowName("Molecular dynamics")
+render_window.SetWindowName("Quake illustration")
 render_window.SetSize(500, 500)
 render_window.AddRenderer(renderer)
 
@@ -156,3 +141,6 @@ interactor.AddObserver("KeyPressEvent", keyboard_interface.keypress)
 interactor.Initialize()
 render_window.Render()
 interactor.Start()
+
+
+#--------------------------End Renderer----------------------------------------
